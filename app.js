7a4cb092d58f27c6890b99681a7373e6d98f21b8
Falguni Shrivastav <99965016+falguni300103@users.noreply.github.com>
2023-03-03 18:32:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+let music=new Audio("music.mp3");
+let turnmusic=new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+let turn="X";
+let complete=false;
+
+//function to change turn
+const changeTurn =()=>{
+    //agr x hai to o warna x
+return turn==="X"?"O":"X";
+}
+
+
+//function to check win
+const checkWin=()=>{
+    let boxtext=document.getElementsByClassName('boxtext');
+let wins=[
+    [0,1,2,5,5,0],
+    [3,4,5,5,15,0],
+    [6,7,8,5,25,0],
+    [0,3,6,-5,15,90],
+    [1,4,7,5,15,90],
+    [2,5,8,15,15,90],
+    [0,4,8,5,15,45],
+    [2,4,6,5,15,135],
+]
+wins.forEach(e=>{
+if((boxtext[e[0]].innerText===boxtext[e[1]].innerText)&&(boxtext[e[2]].innerText===boxtext[e[1]].innerText)&&(boxtext[e[0]].innerText!=="")){
+    document.querySelector('.info').innerText=boxtext[e[0]].innerText + " " + "Won";
+    complete=true;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="200px";
+    document.querySelector(".line").style.width="20vw";
+    document.querySelector(".line").style.transform=`translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`
+}
+})
+}
+
+//game logic
+
+//saare boxes le aao html se as html collection
+music.play();
+let boxes=document.getElementsByClassName("box");
+//boxes ko array mein daldo taki har element access krpaye 
+Array.from(boxes).forEach(element=>{
+    //har ek box k lie boxtext kyuki turn kiska hai wo dekhna hai nd win b
+    let boxtext=element.querySelector('.boxtext');
+    //jaise hi wo box pe click hoga to text change hoga
+    element.addEventListener('click',()=>{
+        //agr khali hai to jo b next turn hai wo ayega
+        if(boxtext.innerText===''){
+            boxtext.innerText=turn;
+            turn = changeTurn();
+            turnmusic.play();
+            checkWin();
+            if(complete==false){
+            //turn for x ya turn for y ye change hoga info mein
+            document.getElementsByClassName("info")[0].innerText="turn for " + turn;
+            }
+        }
+    }
+)})
+
+
+//add onclick listener to reset button
+
+reset.addEventListener('click',()=>{
+    let boxtext=document.querySelectorAll('.boxtext');
+        Array.from(boxtext).forEach(element=>{
+            element.innerText="";
+        });
+        turn="X";
+        complete=false;
+        gameover.play();
+        document.querySelector(".line").style.width="0vw"
+        document.getElementsByClassName("info")[0].innerText="turn for " + turn;
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="0px";
+    }
+)
